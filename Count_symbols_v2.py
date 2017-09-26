56cbeb66ca0f0fb6e99e99decbdd49ec5c54e0f1
omakarovv <makarov.oleg@gmail.com>
2017-09-26 22:04:18 +0300

    Create Count_symbols_v2.py

---
+++ Count_symbols_v2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+### Count symbils in the text ###
+def getAllLettersRus():
+    for item in range(1072, 1104):
+        yield unichr(item)
+
+def getAllSymbolsRus():
+    for symb in range(33, 64):
+        yield unichr(symb)
+
+if __name__ == '__main__':
+    for rus in getAllLettersRus():
+
+        with open("text.txt", "r") as f:
+            file = f.read().decode('cp1251').lower()
+            alaphab = file.count(rus)
+        print("%s - %d" % (rus, alaphab))
+
+    for sym in getAllSymbolsRus():
+        with open("text.txt", "r") as f:
+            file = f.read().decode('cp1251').lower()
+            symbols = file.count(sym)
+            print("%s - %d" % (sym, symbols))
+
+
+
+    print("Total:", len(file) - file.count(' '))
