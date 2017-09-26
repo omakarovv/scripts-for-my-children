d8e4bc8706586f1f70bd4b7479f84cafb9e97647
omakarovv <makarov.oleg@gmail.com>
2017-09-26 22:01:14 +0300

    Create Count_symbols_v1.py

---
+++ Count_symbols_v1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+###Count symbols in the text
+
+def getAllLettersRus():
+    for item in range(1072, 1104):
+        yield unichr(item)
+
+if __name__ == '__main__':
+    with open("text.txt", "r") as f:
+        file = f.read().decode('cp1251').lower()
+        for rus in getAllLettersRus():
+            alaphab = file.count(rus)
+            print("%s - %d" % (rus, alaphab))
+
+        for rus in [',', '.', '-', '!', '?', ':', ';']:
+            alaphab = file.count(rus)
+            print("%s - %d" % (rus, alaphab))
+        print(len(file) - file.count(' '))
