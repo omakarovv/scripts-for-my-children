77e1077da74d0ebe7ba3a6841b27a20ca5119f8e
omakarovv <makarov.oleg@gmail.com>
2017-09-26 23:39:18 +0300

    Update Count_symbols_v1.py

--- Count_symbols_v1.py
+++ Count_symbols_v1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-### Count symbils in the text ###
+### Count symbols in the text ###
 def getAllLettersRus():
     for item in range(1072, 1104):
         yield unichr(item)
@@ -22,9 +22,7 @@ if __name__ == '__main__':
             file = f.read().decode('cp1251').lower()
             symbols = file.count(sym)
             print("%s - %d" % (sym, symbols))
-
-
-
+    
     print("Total:", len(file) - file.count(' '))
 
 ### My version ###
