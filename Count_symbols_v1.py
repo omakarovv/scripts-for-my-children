1447950f20eec54b45a8447dc2ad1d40613cad81
omakarovv <makarov.oleg@gmail.com>
2017-11-03 17:25:58 +0300

    Update Count_symbols_v1.py

--- Count_symbols_v1.py
+++ Count_symbols_v1.py
@@ -1,28 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-### Count symbols in the text ###
 def getAllLettersRus():
     for item in range(1072, 1104):
-        yield unichr(item)
+        yield chr(item)
 
 def getAllSymbolsRus():
     for symb in range(33, 64):
-        yield unichr(symb)
+        yield chr(symb)
 
 if __name__ == '__main__':
     for rus in getAllLettersRus():
-
-        with open("text.txt", "r") as f:
-            file = f.read().decode('cp1251').lower()
+        with open("text.txt", "r", encoding="cp1251") as f:
+            file = f.read().lower()
             alaphab = file.count(rus)
         print("%s - %d" % (rus, alaphab))
 
     for sym in getAllSymbolsRus():
-        with open("text.txt", "r") as f:
-            file = f.read().decode('cp1251').lower()
+        with open("text.txt", "r", encoding="cp1251") as f:
+            file = f.read().lower()
             symbols = file.count(sym)
             print("%s - %d" % (sym, symbols))
-    
-    print("Total:", len(file) - file.count(' '))
 
-### My version ###
+    print("Total:", len(file) - file.count(' '))
